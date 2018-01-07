b140ce6da1bc16d9166eb649715218c61c733278
Billy Rice <william.rice192@gmail.com>
2018-01-07 16:38:10 -0500

    Handled network access permissions Connected to network and retrieved weather data handled exceptions and network errors

--- app/src/main/java/rice/billy/stormy/MainActivity.java
+++ app/src/main/java/rice/billy/stormy/MainActivity.java
@@ -1,12 +1,17 @@
 package rice.billy.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -24,19 +29,58 @@ public class MainActivity extends AppCompatActivity {
         double longitude = -122.4233;
         String forecastUrl = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
 
-        OkHttpClient client = new OkHttpClient();
-       Request request = new Request.
-                Builder().
-                url(forecastUrl).
-                build();
-        Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful()) {
-                Log.d(TAG, response.body().string());
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught ", e);
+        if (isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.
+                    Builder().
+                    url(forecastUrl).
+                    build();
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught ", e);
+                    }
+
+                }
+
+            });
         }
+        else{
+            Toast.makeText(this, R.string.network_unavailable_message, Toast.LENGTH_SHORT).show();
+        }
+
+        Log.d(TAG, "Main UI code is running!");
+
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(),"error_dialog");
+
     }
 }
+
