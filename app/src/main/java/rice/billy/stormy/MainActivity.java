032a92afd60a72b0edb0a2b443ff789db1bd51d9
Billy Rice <william.rice192@gmail.com>
2018-01-07 05:06:04 -0500

    Initial setup of OkHttp client

--- app/src/main/java/rice/billy/stormy/MainActivity.java
+++ app/src/main/java/rice/billy/stormy/MainActivity.java
@@ -2,12 +2,41 @@ package rice.billy.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String apiKey = "e020464846ad3c96fa9e855597380cfd";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forecastUrl = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+       Request request = new Request.
+                Builder().
+                url(forecastUrl).
+                build();
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()) {
+                Log.d(TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Exception caught ", e);
+        }
     }
 }
